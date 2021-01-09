0755606e6d9671539cdd41f2bb89818d6b3e5e17
Roman <55654942+romanprokopuk@users.noreply.github.com>
2021-01-09 23:32:01 +0300

    header, header nav oll displ.

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
     $('.nav-elem-btn__menu').on('click', function() {
       $('.nav-nested').toggleClass('is-toggle')
     })
+
+    $('.search-btn').on('click', function() {
+      $('.search-form').toggleClass('is-toggle');
+      $('.nav-list').toggleClass('is-toggle');
+      $('.search-btn').toggleClass('is-toggle');
+      $('.search-wrap').toggleClass('is-search-btn-toggle');
+    })
   })
   
   
